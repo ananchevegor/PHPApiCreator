30f125e99efd52ff17c11f8e9d7762cce5155f09
ananchev <egorka225522@gmail.com>
2024-09-17 11:33:52 +0200

    add stable

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'ananchev/php-api-creator',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'e2d0d91b45dc4bd2439a60d15cfc6a423182f2cd',
+        'reference' => '5b2f4c744d0851497c7a7c71266ff27e84d0348f',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'ananchev/php-api-creator' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'e2d0d91b45dc4bd2439a60d15cfc6a423182f2cd',
+            'reference' => '5b2f4c744d0851497c7a7c71266ff27e84d0348f',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
